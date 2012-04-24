c72af2efc2bfce518b2e7a2f11d9b4dbaaa7f23a
Nacho <nacho692@gmail.com>
2012-04-24 06:19:31 -0300

    Primer Commit

---
+++ RamonBank/src/com/ramon/ramonbank/MainRamonBank.java
@@ -0,0 +1,9 @@
+package com.ramon.ramonbank;
+
+import com.ramon.ramonbank.utils.ConsoleReader;
+
+public class MainRamonBank {
+	public static void main(String[] args) {
+		System.out.println(ConsoleReader.readLn());
+	}
+}
---
+++ RamonBank/src/com/ramon/ramonbank/utils/ConsoleReader.java
@@ -0,0 +1,11 @@
+package com.ramon.ramonbank.utils;
+
+import java.util.Scanner;
+
+public class ConsoleReader {
+	
+	public static String readLn(){
+     Scanner in = new Scanner(System.in);   
+     return in.nextLine();
+	}
+}
