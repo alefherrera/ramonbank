594f7b8d592dd80b0e0e020ab780bc0cf9f40082
Ale <alef.herrera@gmail.com>
2012-06-27 21:57:00 -0300

    servlets y cosas para que no explote todo

---
+++ RamonBankWeb/src/servlets/servicios/DepositarServlet.java
@@ -0,0 +1,37 @@
+package servlets.servicios;
+
+import java.io.IOException;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ * Servlet implementation class DepositarServlet
+ */
+public class DepositarServlet extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+    /**
+     * @see HttpServlet#HttpServlet()
+     */
+    public DepositarServlet() {
+        super();
+        // TODO Auto-generated constructor stub
+    }
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+	}
+
+}
---
+++ RamonBankWeb/src/servlets/servicios/SolicitarPrestamoServlet.java
@@ -0,0 +1,19 @@
+package servlets.servicios;
+
+import javax.servlet.http.HttpServlet;
+
+/**
+ * Servlet implementation class SolicitarPrestamoServlet
+ */
+public class SolicitarPrestamoServlet extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+    /**
+     * @see HttpServlet#HttpServlet()
+     */
+    public SolicitarPrestamoServlet() {
+        super();
+        // TODO Auto-generated constructor stub
+    }
+
+}
